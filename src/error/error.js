a61b509f5852af4bbcb52820995b0c683e9ff693
dmedv <dmedv@jhu.edu>
2015-06-03 12:12:39 -0400

    Error action URL fixed

--- src/error/error.js
+++ src/error/error.js
@@ -2,7 +2,7 @@ angular.module('angular-login.error', ['angular-login.grandfather'])
 .config(function ($stateProvider) {
   $stateProvider
     .state('app.error', {
-      url: '/login-portal/error/:error',
+      url: '/login-portal/error',
       templateUrl: 'error/error.tpl.html',
       controller: 'ErrorController'
     });
